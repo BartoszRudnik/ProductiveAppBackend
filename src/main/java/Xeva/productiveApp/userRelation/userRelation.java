f6e68c35b0450275f2234c91a8dbc4b583729f37
Wakir <50971678+Wakir@users.noreply.github.com>
2021-04-26 14:45:26 +0200

    Stworzenie tablicy relacji userów

---
+++ src/main/java/Xeva/productiveApp/userRelation/userRelation.java
@@ -0,0 +1,48 @@
+package Xeva.productiveApp.userRelation;
+
+import Xeva.productiveApp.appUser.ApplicationUser;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name="userRelation")
+public class userRelation {
+    @Id
+    @SequenceGenerator(
+            name = "userRelation_sequence",
+            sequenceName = "userRelation_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "userRelation_sequence"
+    )
+    private Long id_userRelation;
+
+    @ManyToOne
+    @JoinColumn(nullable = false, name = "application_user_id")
+    private ApplicationUser user1;
+
+    @ManyToOne
+    @JoinColumn(nullable = false, name = "application_user_id")
+    private ApplicationUser user2;
+
+    @Enumerated(EnumType.STRING)
+    private relationState state = relationState.WAITING;
+    public userRelation(ApplicationUser user1, ApplicationUser user2) {
+        this.user1 = user1;
+        this.user2 = user2;
+    }
+
+    public userRelation(ApplicationUser user1, ApplicationUser user2, relationState state) {
+        this.user1 = user1;
+        this.user2 = user2;
+        this.state = state;
+    }
+}
